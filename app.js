bdd1a24acade82f5ee74b3c9f742a6078b0b045e
Nachiket <nachiketdodia33@gmail.com>
2023-08-26 15:29:11 +0530

    minor bug fix

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
 
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
