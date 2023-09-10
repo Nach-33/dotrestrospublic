201f44b947a37d288b4038cee6da19d8a260f2d1
Harsh Yadav <gabbar@Harshs-MacBook-Air.local>
2023-09-10 16:06:17 +0530

    redirects

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -7,7 +7,7 @@ const authenticate = async (req, res, next) => {
     req.headers.authorization == null ||
     req.headers.authorization == undefined
   ) {
-    return res.json({ redirect: true, uri: "/auth/google" });
+    return res.json({ redirect: true, uri: process.env.BACKEND_LINK });
   }
 
   const token = req.headers.authorization.split(":")[1].split("=")[1];
@@ -15,13 +15,13 @@ const authenticate = async (req, res, next) => {
 
   if (token == null || token == undefined) {
     // console.log("Here Also");
-    return res.json({ redirect: true, uri: "/auth/google" });
+    return res.json({ redirect: true, uri: process.env.BACKEND_LINK });
   }
 
   const user = jwt.verify(token, process.env.JWT_SECRET);
   // console.log("jwt token: ", token);
   // console.log("jwt user: ", user);
-  if (!user) return res.json({ redirect: true, uri: "/auth/google" });
+  if (!user) return res.json({ redirect: true, uri: process.env.BACKEND_LINK });
   req.user = user;
   next();
 };
